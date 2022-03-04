06440f6cdce254d6dd729a6188b05722177f48f4
Tim Kleyersburg <tk@wacg.de>
2022-03-04 13:29:24 +0100

    chore: formatting

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-    const response = await fetch(`http://ws.audioscrobbler.com/2.0/?format=json&method=user.getrecenttracks&user=timmotheus&limit=1&api_key=${LASTFM_API_KEY}`)
+    const response = await fetch(
+        `http://ws.audioscrobbler.com/2.0/?format=json&method=user.getrecenttracks&user=timmotheus&limit=1&api_key=${LASTFM_API_KEY}`,
+    )
 
     return new Response(JSON.stringify(await response.json()), {
         headers: {
