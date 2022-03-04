98ad6923fac60945821ab030e459097a363a0ce8
Tim Kleyersburg <tk@wacg.de>
2022-03-04 13:35:03 +0100

    feat: add cache api

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
+    event.respondWith(handleRequest(event))
 })
 
 /**
- * @param {Request} request
+ * @param event
  */
-async function handleRequest(request) {
-    const response = await fetch(
+async function handleRequest(event) {
+    const cache = caches.default
+
+    let response = await cache.match(event.request)
+    if (response) {
+        return response
+    }
+
+    const scrobble = await fetch(
         `http://ws.audioscrobbler.com/2.0/?format=json&method=user.getrecenttracks&user=timmotheus&limit=1&api_key=${LASTFM_API_KEY}`,
     )
 
-    return new Response(JSON.stringify(await response.json()), {
+    response = new Response(JSON.stringify(await scrobble.json()), {
         headers: {
             'Access-Control-Allow-Origin': '*',
         },
     })
+
+    event.waitUntil(cache.put(event.request, response.clone()))
+
+    return response
 }
